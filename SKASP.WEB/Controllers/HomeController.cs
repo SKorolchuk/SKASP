b6805eb8bee349ce20f167e1d782c2ec132fd907
Sergey Korolchuk <sergey.korolchuk.dev@gmail.com>
2012-12-23 14:31:58 +0300

    First working version of site

--- SKASP.WEB/Controllers/HomeController.cs
+++ SKASP.WEB/Controllers/HomeController.cs
@@ -19,8 +19,6 @@ namespace SKASP.WEB.Controllers
         public HomeController(INewsRepository repository)
         {
             repo = repository;
-            DatabaseTest.TestForumFunctionality();
-            DatabaseTest.TestNewsAndUsrTbl();
         }
 
         public ViewResult Default(string id)
@@ -40,66 +38,6 @@ namespace SKASP.WEB.Controllers
         public ViewResult About()
         {
             return this.View();
-        }
-    }
-    public static class DatabaseTest
-    {
-        /// <summary>
-        /// The test news and usr tbl.
-        /// </summary>
-        public static void TestNewsAndUsrTbl()
-        {
-            using (DatabaseEntities context = new DatabaseEntities())
-            {
-                int count_news = (from msg in context.News select msg).Count();
-                int count_usr = (from usr in context.Usr_lg_data select usr).Count();
-            }
-        }
-
-        /// <summary>
-        /// The test forum functionality.
-        /// </summary>
-        public static void TestForumFunctionality()
-        {
-            using (DatabaseEntities context = new DatabaseEntities())
-            {
-                //context.News.Add(new News{Date = DateTime.Now, ID = 3456, Name = "Test", NewsContent = "Testing"});
-                Usr_lg_data user = context.Usr_lg_data.FirstOrDefault(x => x.id == 1);
-                Theme newTheme = new Theme
-                {
-                    Created_on = DateTime.Now,
-                    ID = (new Random((int)DateTime.Now.Ticks)).Next(),
-                    Creator = user.id,
-                    Name = "Test Theme",
-                    Description = "Hello World",
-                    Subgroup = "All"
-                };
-                Topic newTopic = new Topic()
-                {
-                    Created_on = DateTime.Now,
-                    Creator = user.id,
-                    Description = "Hello world topic",
-                    ID = (new Random((int)DateTime.Now.Ticks)).Next(),
-                    Name = "Test Topic",
-                    Theme = newTheme.ID
-                };
-                Message newMessage = new Message()
-                {
-                    Created_on = DateTime.Now,
-                    Creator = user.id,
-                    ID = (new Random((int)DateTime.Now.Ticks)).Next(),
-                    Text = "hello",
-                    Topic = newTopic.ID
-                };
-                context.Themes.Add(newTheme);
-                context.Topics.Add(newTopic);
-                context.Messages.Add(newMessage);
-                context.SaveChanges();
-                context.Themes.Remove(newTheme);
-                context.Topics.Remove(newTopic);
-                context.Messages.Remove(newMessage);
-                context.SaveChanges();
-            }
         }
     }
 }
