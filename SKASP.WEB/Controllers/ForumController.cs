9360c91d1189965e619493bc6ae71ab03dfb7062
Sergey Korolchuk <sergey.korolchuk.dev@gmail.com>
2013-01-11 01:44:17 +0300

    Issue #5: Realized several forum actions and theme view

--- SKASP.WEB/Controllers/ForumController.cs
+++ SKASP.WEB/Controllers/ForumController.cs
@@ -4,40 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SKASP.WEB.Controllers
-{
-	using SKASP.DOMAIN.Abstract;
+using SKASP.DOMAIN.Abstract;
 using SKASP.DOMAIN.EntitiesModel;
 
+namespace SKASP.WEB.Controllers
+{
+	/// <summary>
+	/// The forum controller.
+	/// </summary>
 	public class ForumController : Controller
 	{
-		private IForumRepository repository;
+		/// <summary>
+		/// The repository.
+		/// </summary>
+		private readonly IForumRepository repository;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ForumController"/> class.
+		/// </summary>
+		/// <param name="repo">
+		/// The repo.
+		/// </param>
 		public ForumController(IForumRepository repo)
 		{
 			repository = repo;
 		}
 
+		/// <summary>
+		/// The themes.
+		/// </summary>
+		/// <returns>
+		/// The <see cref="ActionResult"/>.
+		/// </returns>
 		[Authorize]
         public ActionResult Themes()
         {
             return View(repository.Themes);
         }
 
+		/// <summary>
+		/// The create theme.
+		/// </summary>
+		/// <param name="item">
+		/// The item.
+		/// </param>
+		/// <returns>
+		/// The <see cref="ActionResult"/>.
+		/// </returns>
 		[Authorize]
 		[HttpPost]
 		public ActionResult CreateTheme(Theme item)
 		{
 			if (ModelState.IsValid)
 			{
-				
+				repository.Themes.AddValue(item);
 			}
+			return RedirectToAction("Themes");
 		}
 
+		/// <summary>
+		/// The topics.
+		/// </summary>
+		/// <param name="id">
+		/// The id.
+		/// </param>
+		/// <returns>
+		/// The <see cref="ActionResult"/>.
+		/// </returns>
 		[Authorize]
 		public ActionResult Topics(int id)
 		{
-			List<Topic> topics = repository.Topics.Updates.Where(x => x.ID == id).ToList();
+			List<Topic> topics = repository.Topics.Repository.Where(x => x.ID == id).ToList();
 			return this.View(topics);
 		}
     }
