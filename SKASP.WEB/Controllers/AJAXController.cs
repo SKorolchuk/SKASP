37c0d96902cb021c569fb9281fafab6e59e1a83c
Sergey Korolchuk <sergey.korolchuk.dev@gmail.com>
2013-01-13 20:41:23 +0300

    Practise with Ajax

---
+++ SKASP.WEB/Controllers/AJAXController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SKASP.WEB.Controllers
+{
+	using SKASP.DOMAIN.EntitiesModel;
+
+	public class AJAXController : Controller
+    {
+        //
+        // GET: /AJAX/
+
+        public ActionResult Index()
+        {
+	        News test = new News { ID = 1, Date = DateTime.Now, Name = "Test", NewsContent = "Hello World"};
+            return Json(test, JsonRequestBehavior.AllowGet);
+        }
+
+		[HttpPost]
+		public ActionResult Index(string message)
+		{
+			News test = new News { ID = 1, Date = DateTime.Now, Name = "Test", NewsContent = message };
+			return Json(test, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult Testing()
+		{
+			return this.View();
+		}
+
+		public ActionResult Jquery()
+		{
+			return this.View();
+		}
+    }
+}
---
+++ SKASP.WEB/Scripts/CustomScripts/AJAX.js
@@ -0,0 +1,38 @@
+﻿var JSONLoader = Class.create();
+
+JSONLoader.prototype = {
+	initialize: function() {
+		this.data = {};
+	},
+	Load: function(divId, url) {
+		var divElement = $(divId);
+		var Request = new Ajax.Request(url, {
+			method: "GET",
+			onSuccess: function(json) {
+				var data = json.responseText.evalJSON();
+				var name = data.Name;
+				var description = data.NewsContent;
+				var date = new Date(parseInt(data.Date.replace(/\/Date\((-?\d+)\)\//, '$1')));
+				divElement.innerHTML = '<p>' + name + '</p><p>' + description + '</p><p>' + date.toDateString() + '</p>';
+			}
+		});
+	},
+	Send: function(divId, url, message) {
+		var divElement = $(divId);
+		var Request = new Ajax.Request(url, {
+			method: "POST",
+			parameters: {
+				message: message
+			},
+			onSuccess: function(json) {
+				var data = json.responseText.evalJSON(true);
+				var name = data.Name;
+				var description = data.NewsContent;
+				var date = new Date(data.Date.replace(/\/Date\((-?\d+)\)\//, '$1'));
+				divElement.innerHTML = '<p>' + name + '</p><p>' + description + '</p><p>' + date + '</p>';
+			}
+		});
+	}
+};
+
+var loader = new JSONLoader();
