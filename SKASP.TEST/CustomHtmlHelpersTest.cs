d1ae2628ef7afce5b6d65e04db3b236cb82598d9
Sergey Korolchuk <sergey.korolchuk.dev@gmail.com>
2013-01-15 23:58:43 +0300

    Issue #20: Added Unit test for NewsViewer Helper method

---
+++ SKASP.TEST/CustomHtmlHelpersTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SKASP.TEST
+{
+	using System.Web.Mvc;
+
+	using SKASP.DOMAIN.EntitiesModel;
+	using SKASP.HELPERS;
+
+	[TestClass]
+	public class CustomHtmlHelpersTest
+	{
+		[TestMethod]
+		public void NewsViewer()
+		{
+			News ent = new News()
+				           {
+					           ID = 1,
+							   Date = DateTime.Now,
+							   Name = "Test",
+							   NewsContent = "<i>Hello world!</i>"
+				           };
+			String html = string.Format(
+					"<div class=\"span12 well well-large\">" + "<h2>{0}</h2>" + "<p>{1}</p>" + "<p>{2}</p>"
+					+ "<i class=\"divider\"></i>" + "<p>" + "<a class=\"btn\" href=\"/Home/Default/{3}\">View details &raquo;</a>" + "</p></div>",
+				ent.Name,
+				ent.NewsContent,
+				ent.Date.Value.ToLongDateString() ?? "",
+				ent.ID);
+			Assert.IsTrue(html == (new HtmlHelper<News>(new ViewContext(), new ViewPage())).NewsViewer(ent).ToHtmlString());
+		}
+	}
+}
