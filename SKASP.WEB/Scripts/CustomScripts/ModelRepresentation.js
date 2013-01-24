b7203824a97597fd99c4a8f6f5b40aade9915387
Sergey Korolchuk <skor@scand.com>
2013-01-24 16:41:37 +0200

    Issue #28: Completed Client coverage of Server Model + Inital realization of News ViewModel

--- SKASP.WEB/Scripts/CustomScripts/ModelRepresentation.js
+++ SKASP.WEB/Scripts/CustomScripts/ModelRepresentation.js
@@ -10,6 +10,19 @@ var TableTemplate = Class.Create({
 		return ('<p>' + this.ID + ' ' + this.Name + '</p>');
 	}
 });
+
+var ForumItemTemplate = Class.create(TableTemplate, {
+	initialize: function($super, arg){
+		$super(arg);
+		Creator = arg.Creator;
+		Created_on = ParseDate(arg.Created_on);
+	},
+	Creator: null,
+	Created_on: null,
+	getRepresentation: function($super){
+		return $super() + ('<p>' + this.Creator + ' ' + this.Created_on.toString() + '</p>');
+	}
+});
 //End Base Classes
 
 //Miscellaneous models
@@ -38,12 +51,32 @@ var MessageStorage = Class.create(TableTemplate, {
 		return $super() + ('<p>' + this.Data + '</p><p>' + this.MsgOwner + '</p>');
 	}
 });
+
+var Picture = Class.create({
+	initialize: fucntion(arg){
+		PictureId  = arg.PictureId;
+		PictureSrc = arg.PictureSrc;
+	},
+	PictureId: null,
+	PictureSrc: null
+});
+
+var Statistic = Class.create({
+	initialize: fucntion(arg){
+		ID = arg.ID;
+		Date = ParseDate(arg.Date);
+		Count = arg.Count;
+	},
+	ID: null,
+	Date: null,
+	Count: 0
+});
 //End Miscellaneous models
 
 
 //Forum representation
 
-var Message = Class.create(TableTemplate, {
+var Message = Class.create(ForumItemTemplate, {
 	initialize: function($super, arg){
 		$super(arg);
 		this.Creator = arg.Creator;
@@ -51,27 +84,21 @@ var Message = Class.create(TableTemplate, {
 		this.Text = arg.Text;
 		this.Created_on = ParseDate(arg.Created_on);
 	},
-	Creator: null,
 	Topic: null,
 	Text: null,
-	Created_on: null,
 	getRepresentation: function($super){
 		//ISSUE: Need Proper Realization
 		return $super() + ('<p>' + this.Data + '</p><p>' + this.MsgOwner + '</p>');
 	}
 });
 
-var Topic = Class.create(TableTemplate, {	
+var Topic = Class.create(ForumItemTemplate, {	
 	initialize: function($super, arg){
 		$super(arg);
-		this.Description = arg.Description;
-		this.Creator = arg.Creator;
-		this.Created_on = ParseDate(arg.Created_on);	
+		this.Description = arg.Description;		
 		this.Theme = arg.Theme;
 	},
-	Description: null,
-	Creator: null,
-	Created_on: null,
+	Description: null,	
 	Theme: null,
 	getRepresentation: function($super){
 		//ISSUE: Need Proper Realization
@@ -79,14 +106,34 @@ var Topic = Class.create(TableTemplate, {
 	}
 });
 
-var Theme = Class.create(TableTemplate, {
-	
+var Theme = Class.create(ForumItemTemplate, {
+	initialize: function($super, arg){
+		$super(arg);
+		
+	},
+	Description: null,	
+	Subgroup: null
 });
 
 //End Forum rep.
 
 var UserData = Class.create({
-	initialize: null
+	initialize: function(arg){
+		UserId = arg.UserId;
+		Name = arg.Name;
+		SubName = arg.SubName;
+		BirthDate = arg.BirthDate;
+		Interests = arg.Interests;
+		BirthCity = arg.BirthCity;
+		AvatarId = arg.AvatarId;
+	},
+	UserId: null,
+	Name: null,
+	SubName: null,
+	BirthDate: null,
+	Interests: null,
+	BirthCity: null,
+	AvatarId: null
 });
 
 
--- SKASP.WEB/Scripts/CustomScripts/ViewModels/NewsViewModel.js
+++ SKASP.WEB/Scripts/CustomScripts/ViewModels/NewsViewModel.js
@@ -1,7 +1,13 @@
-function NewsViewModel(news) {
-    var self = this;    
-    self.News = news;
-	self.
+function NewsViewModel() {
+    var self = this;  
+	self.loader = new JSONLoader("all", function(data){
+		self.News = data;
+	});
+    self.UpdateList = function(){
+		self.loader.Load("/NewsApi/GetNews");
+	}
+	self.NewsCountOnPage = 4;
+	self.PageCount = self.News.length / self.NewsCountOnPage;
 }
 
 ko.applyBindings(new NewsViewModel());
